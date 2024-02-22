f545605bd3f6b55cd1729a24ddafb006c291027d
MatthewPCope <matthew.p.cope@gmail.com>
2024-02-21 17:18:33 -0800

    simple count

---
+++ client/src/components/Fun.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Fun() {
+    const [count, setCount] = useState(0)
+
+    const increaseCount = () => {
+        setCount( count + 1 )
+    }
+
+    return (
+        <div>
+            <h1>What's up, I'm Matt</h1>
+            <p>The count is {count}</p>
+            <button onClick = {increaseCount} >Click Me</button>
+        </div>
+    )
+}
+
+export default Fun
